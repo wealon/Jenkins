febe5709758e6fa8b2954f3339dc07d4bac2dd19
wealon <linxianliuzhixiang@163.com>
2016-03-02 20:03:16 +0800

    log

---
+++ Jenkins/Jenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jenkins
+//
+//  Created by wealon on 16/3/2.
+//  Copyright © 2016年 com.mdj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jenkins/Jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jenkins
+//
+//  Created by wealon on 16/3/2.
+//  Copyright © 2016年 com.mdj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
